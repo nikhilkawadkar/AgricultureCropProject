7f28a9c41bda6c61155d52ad645b7c4a71ea32db
nkawadka <nkawadka@LIN25007756.corp.capgemini.com>
2021-11-29 16:54:29 +0530

    initialCommit

---
+++ mongodata/src/main/java/com/example/mongodata/MongodataApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongodata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodataApplication.class, args);
+	}
+
+}
---
+++ mongodata/src/main/java/com/example/mongodata/OrderRepo.java
@@ -0,0 +1,11 @@
+package com.example.mongodata;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+public interface OrderRepo extends MongoRepository< Order, String > {
+	
+	@Query("{ 'itemName' : ?0 }")
+	Order findOrdersByName(String itemName);
+
+}
